a2a1896a81258adb57618861f6899ce41441542b
xiaoxingxingkz <106457371+xiaoxingxingkz@users.noreply.github.com>
2022-05-31 19:51:55 +0800

    Update and rename step2_others_remove.py to step2_others_remove_mri.py

--- scripts/step2_others_remove_mri.py
+++ scripts/step2_others_remove_mri.py
@@ -9,7 +9,6 @@ path_mask = "./script"
 mask = os.path.join(path_mask, 'mask.nii.gz')
 img_mask = nib.load(mask).get_fdata()
 
-
 # MRI data preprocess
 path = "./media/ADNI1_baseline_Processed_AD" 
 files = os.listdir(path)
@@ -26,21 +25,3 @@ for fi in files:
     save_mri = os.path.join(path_son, 'mri_registration_better.nii.gz')
     nib.save(nii_mri, save_mri)
 print('MRI OK')
-
-
-# PET data preprocess
-path = "./media/ADNI1_baseline_Processed_AD" 
-files = os.listdir(path)
-
-for fi in files:
-    path_son = os.path.join(path, fi)
-    fpet = os.path.join(path_son, 'pet_registration.nii.gz')
-    img_pet_add = nib.load(fpet)
-    affine_ = img_pet_add.affine.copy()
-    hdr_ = img_pet_add.header.copy()
-    img_pet = nib.load(fpet).get_fdata()
-    img_pet_r =  img_pet * img_mask
-    nii_pet = nib.Nifti1Image(img_pet_r, affine_, hdr_) #np.eye(4)
-    save_pet = os.path.join(path_son, 'pet_registration_better.nii.gz')
-    nib.save(nii_pet, save_pet)        
-print('PET OK')
